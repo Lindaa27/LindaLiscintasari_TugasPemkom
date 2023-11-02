b7673a49db643e088286d2e1142b6e941324720a
Lindaa27 <147783441+Lindaa27@users.noreply.github.com>
2023-11-02 19:55:58 +0700

    Add files via upload

---
+++ LindaLiscintasari_112020068_Tugas42.m
@@ -0,0 +1,14 @@
+%Linda Liscintasari
+%112020068
+%Pemrograman Komputer D
+
+%matrik 5x5
+A = eye(5,5)
+%matrik 3x3 dengan elemen nol
+B = zeros(3,3)
+%matrik 4x4 elemen 1
+C = ones(4,4)
+% matrik 3x4 elemen terdistribusi antara 0 dan 1
+D = rand(3,4)
+%matrix 3x4  elemen terdistribusi normal
+E = randn(3,4)
